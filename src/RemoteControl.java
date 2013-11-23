d049f9ee12f020bca2a4e2b1fed00f9415d221fe
Xiang Zhang <xzhang@thoughtworks.com>
2013-11-23 22:35:55 +0800

    [ZX] remove temp filed.

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -8,21 +8,19 @@ import java.util.Map;
 
 public class RemoteControl {
 
-    private Light light;
-    private Ceiling ceiling;
-    private Stereo stereo;
-    private final Map<Integer,ICommand> onCommands;
-    private final Map<Integer,ICommand> offCommands;
-    private final Map<Integer,ICommand> onCommands1 = new HashMap<Integer, ICommand>();
-    private final Map<Integer,ICommand> offCommands1 = new HashMap<Integer, ICommand>();
+    private final Map<Integer,ICommand> onCommands = new HashMap<Integer, ICommand>();
+    private final Map<Integer,ICommand> offCommands = new HashMap<Integer, ICommand>();
 
     public RemoteControl(Light light, Ceiling ceiling, Stereo stereo)
     {
-        this.light = light;
-        this.ceiling = ceiling;
-        this.stereo = stereo;
-        onCommands = getOnCommands();
-        offCommands = getOffCommands();
+        setOnCommand(1, new LightOnCommand(light));
+        setOnCommand(2, new CeilingHighCommand(ceiling));
+        setOnCommand(3, new StereoOnCommand(stereo));
+
+        setOffCommand(1, new LightOffCommand(light));
+        setOffCommand(2, new CeilingOffCommand(ceiling));
+        setOffCommand(3, new StereoOffCommand(stereo));
+
     }
 
     public void on(int slot)
@@ -30,16 +28,8 @@ public class RemoteControl {
         onCommands.get(slot).execute();
     }
 
-    private Map<Integer, ICommand> getOnCommands() {
-        setOnCommand(1, new LightOnCommand(light));
-        setOnCommand(2, new CeilingHighCommand(ceiling));
-        setOnCommand(3, new StereoOnCommand(stereo));
-
-        return onCommands1;
-    }
-
     private void setOnCommand(int slot, ICommand lightOnCommand) {
-        onCommands1.put(slot, lightOnCommand);
+        onCommands.put(slot, lightOnCommand);
     }
 
     public void off(int slot)
@@ -47,15 +37,7 @@ public class RemoteControl {
         offCommands.get(slot).execute();
     }
 
-    private Map<Integer, ICommand> getOffCommands() {
-        setOffCommand(1, new LightOffCommand(light));
-        setOffCommand(2, new CeilingOffCommand(ceiling));
-        setOffCommand(3, new StereoOffCommand(stereo));
-
-        return offCommands1;
-    }
-
     private void setOffCommand(int slot, ICommand lightOffCommand) {
-        offCommands1.put(slot, lightOffCommand);
+        offCommands.put(slot, lightOffCommand);
     }
 }
